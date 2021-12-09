9637fc7ffecb4f0fa245e72b9d704c766084ecf2
Fatih Mehmet ARSLAN <contact@fmarslan.com>
2021-12-09 20:01:15 +0300

    Custom repository config

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Run custom python library in the container
 - PACKAGE_NAME: Your package name from global pypi (required)
 - PACKAGE_VERSION: Your package version (optional)
 - REQUIREMENTS_PACKAGES: Your needs packages with space sperator (optional)
+- STARTUP_COMMAND
+- REPOSITORY_URL
+- REPOSITORY_USERNAME
+- REPOSITORY_PASSWORD
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,24 @@
 python3 -m venv /app/venv
 source /app/venv/bin/activate
 
+echo "[pypi]" > /app/venv/.pypirc
+
+if [[ -n "$REPOSITORY_URL" ]]
+then
+    echo "repository: $REPOSITORY_URL" >> /app/venv/.pypirc
+fi
+
+if [[ -n "$REPOSITORY_USERNAME" ]]
+then
+    echo "username: $REPOSITORY_USERNAME" >> /app/venv/.pypirc
+fi
+
+if [[ -n "$REPOSITORY_PASSWORD" ]]
+then
+    echo "password: $REPOSITORY_PASSWORD" >> /app/venv/.pypirc
+fi
+
+
 if [[ -n "$REQUIREMENTS_PACKAGES" ]]
 then
     pip install $REQUIREMENTS_PACKAGES
